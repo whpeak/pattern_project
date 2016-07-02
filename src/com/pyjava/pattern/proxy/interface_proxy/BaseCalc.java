710edf3f79809d6634426317aff5478a55cfe647
hengwang5 <hengwang5@creditease.cn>
2016-07-02 19:18:17 +0800

    代理模式

---
+++ src/com/pyjava/pattern/proxy/interface_proxy/BaseCalc.java
@@ -0,0 +1,19 @@
+package com.pyjava.pattern.proxy.interface_proxy;
+
+/**
+ * Created by wangheng on 16/7/2.
+ */
+
+public class BaseCalc implements CalcInterface {
+    @Override
+    public int calc() {
+
+        int result=(int)(1+Math.random()*(10-1+1));
+        return result;
+    }
+
+    @Override
+    public void sayHello() {
+        System.out.println("hello everyone");
+    }
+}
---
+++ src/com/pyjava/pattern/proxy/interface_proxy/CalcInterface.java
@@ -0,0 +1,13 @@
+package com.pyjava.pattern.proxy.interface_proxy;
+
+/**
+ * 一个接口,
+ * 有一个计算的方法
+ * 有一个打招呼的方法
+ * Created by wangheng on 16/7/2.
+ */
+public interface CalcInterface {
+     int calc();
+     void sayHello();
+}
+
