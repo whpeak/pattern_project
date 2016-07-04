00c6f12394f9985268690bd33382e2eb3597287a
hengwang5 <hengwang5@creditease.cn>
2016-07-04 16:15:59 +0800

    建造者模式

---
+++ src/com/pyjava/pattern/build/HourseMap.java
@@ -0,0 +1,15 @@
+package com.pyjava.pattern.build;
+
+/**
+ * Created by wangheng on 16/7/4.
+ */
+public interface HourseMap {
+
+    void men();
+    void di();
+    void qiang();
+    void chuang();
+
+
+
+}
